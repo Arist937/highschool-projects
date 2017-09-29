3f054d676e64cfea6d6282931ee9523becbb4227
Arist937 <Aristotle937@gmail.com>
2017-09-29 14:27:49 -0400

    Test commit

--- src/unit1/AvgMarks.java
+++ src/unit1/AvgMarks.java
@@ -5,29 +5,32 @@ import codehs.*;
 public class AvgMarks extends ConsoleProgram{
 
     public void run() {
-        double mark1 = readDouble("Enter first mark: ");
-        double mark2 = readDouble("Enter second mark: ");
-        double mark3 = readDouble("Enter third mark: ");
-        double mark4 = readDouble("Enter fourth mark: ");
 
-        double avg = (mark1 + mark2 + mark3 + mark4)/4;
+        String repeatProgram = "Yes";
 
-        System.out.println("\nYour average is: " + avg);
+        while(repeatProgram.equalsIgnoreCase("Yes")) {
+            double mark1 = readDouble("Enter first mark: ");
+            double mark2 = readDouble("Enter second mark: ");
+            double mark3 = readDouble("Enter third mark: ");
+            double mark4 = readDouble("Enter fourth mark: ");
 
-        if(avg >= 90){
-            System.out.println("Wow, you're smart");
-        }
-        else if(avg >= 80){
-            System.out.print("Nice!");
-        }
-        else if(avg >= 70){
-            System.out.println("Decent");
-        }
-        else if(avg >= 60){
-            System.out.println("You're almost there!");
-        }
-        else{
-            System.out.println("...");
+            double avg = (mark1 + mark2 + mark3 + mark4) / 4;
+
+            System.out.println("\nYour average is: " + avg);
+
+            if (avg >= 90) {
+                System.out.println("Wow, you're smart");
+            } else if (avg >= 80) {
+                System.out.print("Nice!");
+            } else if (avg >= 70) {
+                System.out.println("Decent");
+            } else if (avg >= 60) {
+                System.out.println("You're almost there!");
+            } else {
+                System.out.println("... You need work");
+            }
+
+            repeatProgram = readLine("Would you like to repeat the program? ");
         }
     }
 }
