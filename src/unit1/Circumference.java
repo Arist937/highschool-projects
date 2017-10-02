9e3676ccd8a91cdc04ee0099f9dbe6177c8b2943
Arist937 <Aristotle937@gmail.com>
2017-10-02 13:07:48 -0400

    Test commit

--- src/unit1/Circumference.java
+++ src/unit1/Circumference.java
@@ -7,7 +7,7 @@ public class Circumference extends ConsoleProgram {
     public void run() {
         double radius = readDouble("Enter the radius: ");
         double circumferenceLength = Math.PI * 2 * radius;
-        System.out.println("The length of the circumference of your circle is " + circumferenceLength);
+        System.out.println("The length of the circumference of your circle is: " + circumferenceLength);
     }
 }
 
