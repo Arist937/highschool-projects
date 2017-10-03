b62487fcb3297c633fc336492ea9f9b0f1713bc1
Arist937 <Aristotle937@gmail.com>
2017-10-03 14:12:44 -0400

    Test commit

--- src/unit1/Cos3rd.java
+++ src/unit1/Cos3rd.java
@@ -13,7 +13,7 @@ public class Cos3rd extends ConsoleProgram {
     public void run() {
         double a = readDouble("Enter length a: ");
         double b = readDouble("Enter length b: ");
-        double C = readDouble("Enter angle C: ");
+        double C = readDouble("Enter measure of angle C: ");
 
         double cLength = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2) - 2*a*b*Math.cos(Math.toRadians(C)));
         System.out.println(cLength);
