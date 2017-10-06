5612554374a10b491fa7b02cb17326cb0dfeaf94
Arist937 <Aristotle937@gmail.com>
2017-10-06 13:14:31 -0400

    livehack

---
+++ src/unit1/otherStuff/Fibonacci.java
@@ -0,0 +1,27 @@
+/*
+ @author: Arist
+*/
+
+package unit1.otherStuff;
+import codehs.*;
+
+
+public class Fibonacci extends ConsoleProgram{
+
+    public void run() {
+        int a = 1;
+        int b = 1;
+        int c = 1;
+        int d = 1;
+
+
+        System.out.println("-------------");
+        System.out.println("| " + a + " | "+ b + " | " + c + " |");
+        System.out.println("-------------");
+        System.out.println("| " + a + " | "+ b + " | " + c + " |");
+        System.out.println("-------------");
+        System.out.println("| " + a + " | "+ b + " | " + c + " |");
+        System.out.println("-------------");
+    }
+}
+
