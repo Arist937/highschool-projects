02c6e28272820a20e4e869be51c9ee4b1376f6f7
Arist937 <31931766+Arist937@users.noreply.github.com>
2017-10-09 12:26:06 -0400

    Tic-Tac-Toe

--- src/unit1/otherStuff/Fibonacci.java
+++ src/unit1/otherStuff/Fibonacci.java
@@ -9,19 +9,128 @@ import codehs.*;
 public class Fibonacci extends ConsoleProgram{
 
     public void run() {
-        int a = 1;
-        int b = 1;
-        int c = 1;
-        int d = 1;
+        String a1 = "a1";
+        String a2 = "a2";
+        String a3 = "a3";
+        String b1 = "b1";
+        String b2 = "b2";
+        String b3 = "b3";
+        String c1 = "c1";
+        String c2 = "c2";
+        String c3 = "c3";
 
+        while(true) {
 
-        System.out.println("-------------");
-        System.out.println("| " + a + " | "+ b + " | " + c + " |");
-        System.out.println("-------------");
-        System.out.println("| " + a + " | "+ b + " | " + c + " |");
-        System.out.println("-------------");
-        System.out.println("| " + a + " | "+ b + " | " + c + " |");
-        System.out.println("-------------");
+            System.out.println("----------------");
+            System.out.println("| " + a1 + " | " + b1 + " | " + c1 + " |");
+            System.out.println("----------------");
+            System.out.println("| " + a2 + " | " + b2 + " | " + c2 + " |");
+            System.out.println("----------------");
+            System.out.println("| " + a3 + " | " + b3 + " | " + c3 + " |");
+            System.out.println("----------------");
+
+            String playerOneDecision = readLine("Player one, which square would you like to modify? ");
+            String playerTwoDecision = "c3";
+
+            if(playerOneDecision.equals("a1")){
+                a1 = "X ";
+            }
+            else if(playerOneDecision.equals("a2")){
+                a2 = "X ";
+            }
+            else if(playerOneDecision.equals("a3")){
+                a3 = "X ";
+            }
+             else if(playerOneDecision.equals("b1")){
+                b1 = "X ";
+            }
+            else if(playerOneDecision.equals("b2")){
+                b2 = "X ";
+            }
+            else if(playerOneDecision.equals("b3")){
+                b3 = "X ";
+            }
+            else if(playerOneDecision.equals("c1")){
+                c1 = "X ";
+            }
+            else if(playerOneDecision.equals("c2")){
+                c2 = "X ";
+            }
+
+            else if(playerOneDecision.equals("c3")){
+                c3 = "X ";
+            }
+            else{
+                System.out.println("Not a valid answer, please enter another number");
+                playerOneDecision = readLine("Which square would you like to modify?");
+            }
+
+            if(playerTwoDecision.equals("a1")){
+                a1 = "O ";
+            }
+            else if(playerTwoDecision.equals("a2")){
+                a2 = "O ";
+            }
+            else if(playerTwoDecision.equals("a3")){
+                a3 = "O ";
+            }
+            else if(playerTwoDecision.equals("b1")){
+                b1 = "O ";
+            }
+            else if(playerTwoDecision.equals("b2")){
+                b2 = "O ";
+            }
+            else if(playerTwoDecision.equals("b3")){
+                b3 = "O ";
+            }
+            else if(playerTwoDecision.equals("c1")){
+                c1 = "O ";
+            }
+            else if(playerTwoDecision.equals("c2")){
+                c2 = "O ";
+            }
+
+            else if(playerTwoDecision.equals("c3")){
+                c3 = "O ";
+            }
+            else{
+                System.out.println("Not a valid answer, please enter another number");
+                playerOneDecision = readLine("Which square would you like to modify?");
+            }
+
+            if(a1.equals(b1) && a1.equals(c1) && a1.equals("X")){
+                System.out.println("Player One Wins!");
+                break;
+            }
+            if(a2 == b2 && a2 == c2 && a1.equals("X")){
+                System.out.println("Player One Wins!");
+                break;
+            }
+            if(a3 == b3 && a3 == c3 && a1.equals("X")){
+                System.out.println("Player One Wins!");
+                break;
+            }
+            if(a1 == a2 && a1 == a3 && a1.equals("X")){
+                System.out.println("Player One Wins!");
+                break;
+            }
+            if(b1 == b2 && b1 == b3 && a1.equals("X")){
+                System.out.println("Player One Wins!");
+                break;
+            }
+            if(c1 == c2 && c1 == c3 && a1.equals("X")){
+                System.out.println("Player One Wins!");
+                break;
+            }
+            if(a1 == b2 && a1 == c3 && a1.equals("X")){
+                System.out.println("Player One Wins!");
+                break;
+            }
+            if(c1 == b2 && c1 == a3 && a1.equals("X")){
+                System.out.println("Player One Wins!");
+                break;
+            }
+        }
     }
 }
 
---
+++ src/unit1/otherStuff/TicTacToe.java
@@ -0,0 +1,236 @@
+/*
+ @author: Arist
+*/
+
+package unit1.otherStuff;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+public class TicTacToe{
+
+    public static ArrayList<String> obj = new ArrayList<String>();
+    public static Scanner read = new Scanner(System.in);
+    public static int victor = 0;
+
+    public static void main(String []args) {
+        int x = 0;
+
+
+        firstSetUp();
+        game();
+    }
+
+    public static void game() {
+    resetBoard();
+    victor = 0;
+
+    while (victor == 0) {
+        while (victor == 0) {
+
+            drawGame();
+
+            playerOne();
+
+            result();
+            break;
+        }
+        while (victor == 0) {
+
+            drawGame();
+
+            playerTwo();
+
+            result();
+            break;
+        }
+    }
+
+    repeatProgram();
+}
+
+    public static void drawGame(){
+        System.out.println("----------------");
+        System.out.println("a | " + obj.get(0) + " | " + obj.get(1) + " | " + obj.get(2) + " |");
+        System.out.println("----------------");
+        System.out.println("b | " + obj.get(3) + " | " + obj.get(4) + " | " + obj.get(5) + " |");
+        System.out.println("----------------");
+        System.out.println("c | " + obj.get(6) + " | " + obj.get(7) + " | " + obj.get(8) + " |");
+        System.out.println("----------------");
+        System.out.println("    1   2   3");
+    }
+
+    public static void playerOne(){
+
+        System.out.print("Player X, which square would you like to modify? ");
+        String playerOneDecision = read.nextLine();
+
+        while(true) {
+            if (playerOneDecision.equalsIgnoreCase("a1") && obj.get(0).equalsIgnoreCase(" ")) {
+                obj.set(0, "X");
+                break;
+            } else if (playerOneDecision.equalsIgnoreCase("a2") && obj.get(1).equalsIgnoreCase(" ")) {
+                obj.set(1, "X");
+                break;
+            } else if (playerOneDecision.equalsIgnoreCase("a3") && obj.get(2).equalsIgnoreCase(" ")) {
+                obj.set(2, "X");
+                break;
+            } else if (playerOneDecision.equalsIgnoreCase("b1") && obj.get(3).equalsIgnoreCase(" ")) {
+                obj.set(3, "X");
+                break;
+            } else if (playerOneDecision.equalsIgnoreCase("b2") && obj.get(4).equalsIgnoreCase(" ")) {
+                obj.set(4, "X");
+                break;
+            } else if (playerOneDecision.equalsIgnoreCase("b3") && obj.get(5).equalsIgnoreCase(" ")) {
+                obj.set(5, "X");
+                break;
+            } else if (playerOneDecision.equalsIgnoreCase("c1") && obj.get(6).equalsIgnoreCase(" ")) {
+                obj.set(6, "X");
+                break;
+            } else if (playerOneDecision.equalsIgnoreCase("c2") && obj.get(7).equalsIgnoreCase(" ")) {
+                obj.set(7, "X");
+                break;
+            } else if (playerOneDecision.equalsIgnoreCase("c3") && obj.get(8).equalsIgnoreCase(" ")) {
+                obj.set(8, "X");
+                break;
+            } else {
+                System.out.print("Sorry, that is not a valid , please enter a valid square ");
+                playerOneDecision = read.nextLine();
+            }
+        }
+    }
+
+    public static void playerTwo(){
+        System.out.print("Player O, which square would you like to modify? ");
+        String playerTwoDecision = read.nextLine();
+
+        while(true) {
+            if (playerTwoDecision.equalsIgnoreCase("a1") && obj.get(0).equalsIgnoreCase(" ")) {
+                obj.set(0, "O");
+                break;
+            } else if (playerTwoDecision.equalsIgnoreCase("a2") && obj.get(1).equalsIgnoreCase(" ")) {
+                obj.set(1, "O");
+                break;
+            } else if (playerTwoDecision.equalsIgnoreCase("a3") && obj.get(2).equalsIgnoreCase(" ")) {
+                obj.set(2, "O");
+                break;
+            } else if (playerTwoDecision.equalsIgnoreCase("b1") && obj.get(3).equalsIgnoreCase(" ")) {
+                obj.set(3, "O");
+                break;
+            } else if (playerTwoDecision.equalsIgnoreCase("b2") && obj.get(4).equalsIgnoreCase(" ")) {
+                obj.set(4, "O");
+                break;
+            } else if (playerTwoDecision.equalsIgnoreCase("b3") && obj.get(5).equalsIgnoreCase(" ")) {
+                obj.set(5, "O");
+                break;
+            } else if (playerTwoDecision.equalsIgnoreCase("c1") && obj.get(6).equalsIgnoreCase(" ")) {
+                obj.set(6, "O");
+                break;
+            } else if (playerTwoDecision.equalsIgnoreCase("c2") && obj.get(7).equalsIgnoreCase(" ")) {
+                obj.set(7, "O");
+                break;
+            } else if (playerTwoDecision.equalsIgnoreCase("c3") && obj.get(8).equalsIgnoreCase(" ")) {
+                obj.set(8, "O");
+                break;
+            } else {
+                System.out.print("Sorry, that is not a valid , please enter a valid square ");
+                playerTwoDecision = read.nextLine();
+            }
+        }
+    }
+
+    public static void resetBoard(){
+        obj.set(0, " ");
+        obj.set(1, " ");
+        obj.set(2, " ");
+        obj.set(3, " ");
+        obj.set(4, " ");
+        obj.set(5, " ");
+        obj.set(6, " ");
+        obj.set(7, " ");
+        obj.set(8, " ");
+    }
+
+    public static void firstSetUp(){
+        obj.add(" ");
+        obj.add(" ");
+        obj.add(" ");
+        obj.add(" ");
+        obj.add(" ");
+        obj.add(" ");
+        obj.add(" ");
+        obj.add(" ");
+        obj.add(" ");
+    }
+
+    public static void result(){
+        if (obj.get(0).equalsIgnoreCase(obj.get(1)) && obj.get(0).equalsIgnoreCase(obj.get(2)) && !obj.get(0).equals(" ")){
+            drawGame();
+            System.out.println("Player " + obj.get(0) + " Wins!");
+            victor = 1;
+        }
+        else if(obj.get(3).equalsIgnoreCase(obj.get(4)) && obj.get(3).equalsIgnoreCase(obj.get(5)) && !obj.get(3).equals(" ")){
+            drawGame();
+            System.out.println("Player " + obj.get(3) + " Wins!");
+            victor = 1;
+        }
+        else if(obj.get(6).equalsIgnoreCase(obj.get(7)) && obj.get(6).equalsIgnoreCase(obj.get(8)) && !obj.get(6).equals(" ")){
+            drawGame();
+            System.out.println("Player " + obj.get(6) + " Wins!");
+            victor = 1;
+        }
+        else if(obj.get(0).equalsIgnoreCase(obj.get(3)) && obj.get(0).equalsIgnoreCase(obj.get(6)) && !obj.get(0).equals(" ")){
+            drawGame();
+            System.out.println("Player " + obj.get(0) + " Wins!");
+            victor = 1;
+        }
+        else if(obj.get(1).equalsIgnoreCase(obj.get(4)) && obj.get(1).equalsIgnoreCase(obj.get(7)) && !obj.get(1).equals(" ")){
+            drawGame();
+            System.out.println("Player " + obj.get(1) + " Wins!");
+            victor = 1;
+        }
+        else if(obj.get(2).equalsIgnoreCase(obj.get(5)) && obj.get(2).equalsIgnoreCase(obj.get(8)) && !obj.get(2).equals(" ")){
+            drawGame();
+            System.out.println("Player " + obj.get(2) + " Wins!");
+            victor = 1;
+        }
+        else if(obj.get(0).equalsIgnoreCase(obj.get(4)) && obj.get(0).equalsIgnoreCase(obj.get(8)) && !obj.get(0).equals(" ")){
+            drawGame();
+            System.out.println("Player " + obj.get(0) + " Wins!");
+            victor = 1;
+        }
+        else if(obj.get(2).equalsIgnoreCase(obj.get(4)) && obj.get(2).equalsIgnoreCase(obj.get(6)) && !obj.get(2).equals(" ")){
+            drawGame();
+            System.out.println("Player " + obj.get(2) + " Wins!");
+            victor = 1;
+        }
+        else if(!obj.get(0).equalsIgnoreCase(" ") && !obj.get(1).equalsIgnoreCase(" ") && !obj.get(2).equalsIgnoreCase(" ") &&
+                !obj.get(3).equalsIgnoreCase(" ") && !obj.get(4).equalsIgnoreCase(" ") && !obj.get(5).equalsIgnoreCase(" ") &&
+                !obj.get(6).equalsIgnoreCase(" ") && !obj.get(7).equalsIgnoreCase(" ") && !obj.get(8).equalsIgnoreCase(" ")){
+
+            System.out.println("It's a draw!");
+            victor = 1;
+        }
+    }
+
+    public static void repeatProgram(){
+        while(true) {
+            System.out.println("Would you like to play again?");
+            String repeatProgram = read.nextLine();
+
+            while (true) {
+                if (repeatProgram.equalsIgnoreCase("Yes")) {
+                    System.out.println("Great, have fun!");
+                    game();
+                    break;
+                } else if (repeatProgram.equalsIgnoreCase("No")) {
+                    System.out.println("I hope you had fun! See you next time;");
+                    System.exit(0);
+                } else {
+                    System.out.println("I'm sorry, I didn't get that, would you like to play again?");
+                    repeatProgram = read.nextLine();
+                }
+            }
+        }
+    }
+}
