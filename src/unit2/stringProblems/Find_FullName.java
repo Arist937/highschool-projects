5c7dc3747cce3315d536526985bca69786913512
Arist937 <Aristotle937@gmail.com>
2017-11-07 12:37:21 -0500

    String Problems

--- src/unit2/stringProblems/Find_FullName.java
+++ src/unit2/stringProblems/Find_FullName.java
@@ -4,9 +4,9 @@ import codehs.*;
 public class Find_FullName extends ConsoleProgram {
     public void run(){
         String fullName = readLine("Enter your full name: ");
-        int locationFirstName = fullName.indexOf(" ");
+        int endOfFirstName = fullName.indexOf(" ");
 
-        System.out.println("The first name is: " + fullName.substring(0, locationFirstName));
-        System.out.println("The last name is: " + fullName.substring(locationFirstName + 1, fullName.length()));
+        System.out.println("The first name is: " + fullName.substring(0, endOfFirstName));
+        System.out.println("The last name is: " + fullName.substring(endOfFirstName + 1, fullName.length()));
     }
 }
