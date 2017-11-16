f94c22550dc124f9fa0c061cfc10112c7aefcfbb
Arist937 <Aristotle937@gmail.com>
2017-11-16 13:45:58 -0500

    entered code to get user input in C1BackToBack

--- src/unit2/Assignment/C1BackToBack.java
+++ src/unit2/Assignment/C1BackToBack.java
@@ -5,7 +5,13 @@ import codehs.*;
 public class C1BackToBack extends ConsoleProgram {
     public void run() {
         // Variable userWord to get input and newWord that stores the final word
-        String userWord;
+        String userWord = "";
         String newWord;
+
+        // While loop to ensure that user cannot input nothing
+        while (userWord == "") {
+            // Get input from the user
+            userWord = readLine("Enter a word: ");
+        }
     }
 }
