aeb98b97bb35bb710dfe2e6c28d3f807cbaf3e40
Arist937 <Aristotle937@gmail.com>
2017-11-16 14:10:34 -0500

    added for loop to add the nth char to a string in C2EveryNth

--- src/unit2/Assignment/C2EveryNth.java
+++ src/unit2/Assignment/C2EveryNth.java
@@ -8,5 +8,15 @@ public class C2EveryNth extends ConsoleProgram {
         String userWord;
         String newWord = "";
         int valueN;
+
+        // Get a word from the user and the value of n
+        userWord = readLine("Enter your word: ");
+        valueN = readInt("Enter the value of n: ");
+
+        // Loop that runs for the length of the word that the user entered divided by 3
+        for (int i = 0; i < userWord.length(); i += valueN) {
+            // Add the valueNth char of userWord to newWord
+            newWord = newWord + userWord.charAt(i);
+        }
     }
 }
