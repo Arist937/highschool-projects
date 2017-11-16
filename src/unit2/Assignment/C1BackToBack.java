0625ead3c1cb96bc9205ee54fee213d32302fab3
Arist937 <Aristotle937@gmail.com>
2017-11-16 13:43:31 -0500

    created variables in C1BackToBack

--- src/unit2/Assignment/C1BackToBack.java
+++ src/unit2/Assignment/C1BackToBack.java
@@ -2,5 +2,10 @@ package unit2.Assignment;
 
 import codehs.*;
 
-public class C1BackToBack {
+public class C1BackToBack extends ConsoleProgram {
+    public void run() {
+        // Variable userWord to get input and newWord that stores the final word
+        String userWord;
+        String newWord;
+    }
 }
