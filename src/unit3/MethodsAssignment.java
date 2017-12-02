bd56bef02b3e702092e93827754f25c6d33dc217
Arist <Aristotle937@gmail.com>
2017-12-01 23:28:46 -0500

    created var to store lcm and begun trying to find the lcm

--- src/unit3/MethodsAssignment.java
+++ src/unit3/MethodsAssignment.java
@@ -2,9 +2,17 @@ package unit3;
 
 public class MethodsAssignment {
     public static void main(String[] args) {
+        System.out.println(lcm(60, 30));
     }
 
     public static int lcm(int val1, int val2) {
-        
+        // Declare variable lcmVal to store lcm
+        int lcmVal = 0;
+
+        while (val1 % val2 != 0){
+            lcmVal += val1;
+        }
+
+        return lcmVal;
     }
 }
