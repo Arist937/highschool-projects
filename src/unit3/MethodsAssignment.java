531be8feb0a0d1a8d30cc19347871f1f31ea693e
Arist <Aristotle937@gmail.com>
2017-12-01 23:35:26 -0500

    found way to find lcm, only works if val1 is bigger

--- src/unit3/MethodsAssignment.java
+++ src/unit3/MethodsAssignment.java
@@ -2,14 +2,14 @@ package unit3;
 
 public class MethodsAssignment {
     public static void main(String[] args) {
-        System.out.println(lcm(60, 30));
+        System.out.println(lcm(123, 31));
     }
 
     public static int lcm(int val1, int val2) {
         // Declare variable lcmVal to store lcm
-        int lcmVal = 0;
+        int lcmVal = val1;
 
-        while (val1 % val2 != 0){
+        while (lcmVal % val2 != 0){
             lcmVal += val1;
         }
 
