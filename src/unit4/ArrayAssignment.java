21744295842702e91bec170002da6bfaafc20770
Arist <Aristotle937@gmail.com>
2018-01-06 14:34:43 -0500

    unluckyOne finished and tested, works

--- src/unit4/ArrayAssignment.java
+++ src/unit4/ArrayAssignment.java
@@ -7,16 +7,24 @@ package unit4;
 
 public class ArrayAssignment {
     public static void main(String[] args) {
+        int[] testArrayOne = {1, 3, 4, 5, 6, 2, 3};
+        System.out.println(unluckyOne(testArrayOne));
     }
 
     public static boolean unluckyOne(int[] numArray) {
         // declare boolean isUnluckyOne
-        boolean isUnluckyOne;
+        boolean isUnluckyOne = false;
 
         // if the first 2 values of an array are 1 followed by a 3, set isUnluckyOne to true
         if(numArray[0] == 1 && numArray[1] == 3) {
             isUnluckyOne = true;
 
         }
+        // if the last two values of an array are 1 followed by a 3, set isUnluckyOne to true
+        else if (numArray[numArray.length - 2] == 1 && numArray[numArray.length - 1] == 3){
+            isUnluckyOne = true;
+        }
+
+        return isUnluckyOne;
     }
 }
