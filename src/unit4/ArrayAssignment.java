77f90f0e033511448c061c76d49c2e5c2dffdfa5
Arist <Aristotle937@gmail.com>
2018-01-06 14:54:51 -0500

    added code to find the number of closeby numbers in closeBy2

--- src/unit4/ArrayAssignment.java
+++ src/unit4/ArrayAssignment.java
@@ -66,5 +66,15 @@ public class ArrayAssignment {
     }
 
     public static int closeBy2(int[] nums1, int[] nums2){
+        // declare integer variable numOfCloseBy
+        int numOfCloseBy = 0;
+
+        // loop for the length of array nums1
+        for(int i = 0; i < nums1.length; i++){
+            // if nums1 and nums2 at index i differ by 2 or 1, increment numOfCloseBy
+            if(nums1[i] - nums2[i] <= 2 && nums1[i] - nums2[i] >= -2){
+                numOfCloseBy++;
+            }
+        }
     }
 }
