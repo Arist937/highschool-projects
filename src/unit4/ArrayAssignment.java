35ea75fb9d7149a5875e4ff9fbbe245b9f571334
Arist <Aristotle937@gmail.com>
2018-01-06 14:25:35 -0500

    Added program header and started unluckyOne

---
+++ src/unit4/ArrayAssignment.java
@@ -0,0 +1,14 @@
+package unit4;
+
+/**
+ * A program ArrayAssignment.java that contains and tests four methods unluckyOne, after4, closeBy2 and diagonal
+ * @author: A. Xu
+ */
+
+public class ArrayAssignment {
+    public static void main(String[] args) {
+    }
+
+    public static boolean unluckyOne(int[] numArray) {
+    }
+}
