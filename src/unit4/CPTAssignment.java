c3843ac519879d71910c982d83803721537f699a
Arist937 <Aristotle937@gmail.com>
2018-01-11 12:58:27 -0500

    started setting up basic CPT structure

---
+++ src/unit4/CPTAssignment.java
@@ -0,0 +1,13 @@
+package unit4;
+
+public class CPTAssignment {
+    static String[] roomNames;
+    static String[] roomDescriptions;
+    static int[][] roomDirections;
+
+    public static void main(String[] args){
+        roomNames = new String[]{"Hallway B", "Hallway A", "Hallway C", "Kitchen", "Dining Room"};
+        roomDescriptions = new String[]{"A hallway", "A hallway", "A hallway", "A kitchen", "A dining room"};
+        roomDirections = new int[5][4];
+    }
+}
