f44143b57a760fdc57cd55141cd8911fb712a894
Arist <Aristotle937@gmail.com>
2018-01-23 19:52:00 -0500

    recreated building layout for floor 1 (new room descriptions, names, and directions)

--- src/unit4/CPTAssignment.java
+++ src/unit4/CPTAssignment.java
@@ -14,33 +14,41 @@ public class CPTAssignment {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
 
-        roomNames1 = new String[]{"Starting Position", "Hallway A1", "Hallway A2", "Hallway B1", "Hallway B2", "Kitchen",
-                "Dining Room", "Hallway C2", "Hallway C1"};
-        roomDescriptions1 = new String[]{"You find yourself in the middle of a hallway. It leads east and west",
-                "You find yourself on the right side of your starting hallway. To the " +
-                        "North, there is a dining room and the east leads to another hallway",
-                "You find yourself on the left side of your starting hallway. The west " +
-                        "leads to another hallway",
-                "You find yourself in another hallway with the east leading back and the " +
-                        "North leads further down the hallway",
-                "You find yourself further down the hallway with a kitchen to the east" +
-                        "and a set of stairs",
-                "You are in a kitchen, with a dining room to the east and a hallway to " +
-                        "the west",
-                "You find yourself in a dining room with a hallway to the south and a kitchen " +
-                        "to the west",
-                "You find yourself in another hallway. It extends south.",
-                "You find yourself in a hallway that extends north"};
+        roomNames1 = new String[]{"Southwest Tower", "Foyer", "Enchanting Room", "Southeast Tower", "Alchemy Room",
+                "Courtyard", "Central Tower", "Hallway A", "Dining Room", "Kitchen", "Northwest Tower", "Library",
+                "Hallway B", "Chapel", "Northeast Tower"};
+        roomDescriptions1 = new String[]
+                {"You make your way into the southwestern tower. The Dining room lies north.",
+                        "You find yourself in the Foyer of a castle. There is set of stairs that bring you to the second floor of the castle. In the north, there is a Hallway and to the east lies the enchanting room",
+                        "You find yourself in the enchanting room. To the north, there is an alchemy room, to the south, there is the southeastern tower and to the west, there is the foyer of the castle",
+                        "You find yourself in the southeastern tower of the castle. To the north, there is an enchanting room.",
+                        "You make your way into the alchemy room. To the west lies a hallway and the enchanting room is south of here.",
+                        "You make your way into the courtyard. To the east lies the central tower and you can return into the castle through the west. Near the entrance of the central tower, you notice a skeleton that seemed to have been crawling for life in its final moments.",
+                        "You find yourself in the central tower. To the west, you can find courtyard",
+                        "You make your way into a hallway. To the north, it leads into another hallway and to the west lies a dining room.",
+                        "You make your way into a dining room. To the south lies the southwestern tower and the kitchen is to the north",
+                        "You find yourself in a kitchen. To the south lies the dining room, the Northwestern Tower to the north and a hallway to the east",
+                        "You make your way into the Northwestern Tower. To the south lies the kitchen.",
+                        "You find yourself in a library. To the south is a hallway and the chapel is to the east",
+                        "You make your way into a hallway. There lies a library to the north, a kitchen to the west and the alchemy room to the east",
+                        "You are in the chapel. To the west is the library and to the north is the Northeastern Tower",
+                        "You find yourself in the northeastern tower of the castle. To the south lies the chapel."};
 
-        roomDirections1[0] = new int[]{-1, 1, -1, 2};
-        roomDirections1[1] = new int[]{6, 8, -1, 2};
-        roomDirections1[2] = new int[]{-1, 1, -1, 3};
-        roomDirections1[3] = new int[]{4, 2, -1, -1};
-        roomDirections1[4] = new int[]{-1, 5, 3, -1};
-        roomDirections1[5] = new int[]{-1, 6, -1, 4};
-        roomDirections1[6] = new int[]{-1, 7, 1, 5};
-        roomDirections1[7] = new int[]{-1, -1, 8, 6};
-        roomDirections1[8] = new int[]{7, -1, -1, 1};
+        roomDirections1[0] = new int[]{8, -1, -1, -1};
+        roomDirections1[1] = new int[]{7, 2, -1, -1};
+        roomDirections1[2] = new int[]{4, -1, 3, 1};
+        roomDirections1[3] = new int[]{2, -1, -1, -1};
+        roomDirections1[4] = new int[]{-1, 12, 2, -1};
+        roomDirections1[5] = new int[]{-1, 6, -1, 7};
+        roomDirections1[6] = new int[]{-1, -1, -1, 5};
+        roomDirections1[7] = new int[]{12, 5, 1, 8};
+        roomDirections1[8] = new int[]{9, 7, 0, -1};
+        roomDirections1[9] = new int[]{10, 12, 8, -1};
+        roomDirections1[10] = new int[]{-1, -1, 9, -1};
+        roomDirections1[11] = new int[]{-1, 13, 12, -1};
+        roomDirections1[12] = new int[]{11, 4, 7, 9};
+        roomDirections1[13] = new int[]{14, -1, -1, 11};
+        roomDirections1[14] = new int[]{-1, -1, 13, -1};
 
         roomNames2 = new String[]{"Hallway D1", "Hallway D2", "Bedroom A", "Bedroom B"};
         roomDescriptions2 = new String[]{"You find yourself in a hallway, it extends to the east and a bedroom lies" +
