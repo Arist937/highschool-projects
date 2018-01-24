8fe1c30a7077a65ad2ea915ff6ebe8b2641b14b1
Arist <Aristotle937@gmail.com>
2018-01-23 19:02:46 -0500

    new roomNames and roomDescriptions

--- src/unit4/CPTAssignment.java
+++ src/unit4/CPTAssignment.java
@@ -6,8 +6,22 @@ public class CPTAssignment {
     static int[][] roomDirections;
 
     public static void main(String[] args){
-        roomNames = new String[]{"Hallway B", "Hallway A", "Hallway C", "Kitchen", "Dining Room"};
-        roomDescriptions = new String[]{"A hallway", "A hallway", "A hallway", "A kitchen", "A dining room"};
-        roomDirections = new int[5][4];
+        roomNames = new String[]{"Starting Position", "Hallway A1", "Hallway A2", "Hallway B1", "Hallway B2", "Kitchen",
+                "Dining Room", "Hallway C2", "Hallway C1"};
+        roomDescriptions = new String[]{"You find yourself in the middle of a hallway. It leads east and west",
+                "You find yourself on the right side of your starting hallway. To the " +
+                        "North, there is a dining room and the east leads to another hallway",
+                "You find yourself on the left side of your starting hallway. The west " +
+                        "leads to another hallway",
+                "You find yourself in another hallway with the east leading back and the " +
+                        "North leads further down the hallway",
+                "You find yourself further down the hallway with a kitchen to the east" +
+                        "and a set of stairs",
+                "You are in a kitchen, with a dining room to the east and a hallway to " +
+                        "the west",
+                "You find yourself in a dining room with a hallway to the south and a kitchen " +
+                        "to the west",
+                "You find yourself in another hallway. It extends south.",
+                "You find yourself in a hallway that extends north"};
     }
 }
