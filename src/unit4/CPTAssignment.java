b357d2ff38f1929ebe827f09cf65bf8816d4f353
Arist <Aristotle937@gmail.com>
2018-01-24 18:04:55 -0500

    arrays are intialized in a separate method now

--- src/unit4/CPTAssignment.java
+++ src/unit4/CPTAssignment.java
@@ -19,82 +19,6 @@
         public static void main(String[] args) {
             int booksRead = 0;
 
-            // hasPotions, Alchemy ingredients
-            roomState1[4] = new boolean[]{false, false};
-            // isSkeletonExamined, Skeleton's Journal
-            roomState1[5] = new boolean[]{false, false};
-            // hasEaten plate of food
-            roomState1[8] = new boolean[]{false};
-            // Cooking Ingredients, hasCookedMeal, hasStew
-            roomState1[9] = new boolean[]{false, false, false};
-            // hasSwordUpgrade, hasArmorUpgrade, hasEnchantingKnowledge, hasAlchemyKnowledge
-            roomState1[11] = new boolean[]{false, false, false, false};
-            // hasReceivedBlessing
-            roomState1[13] = new boolean[]{false};
-
-            // hasShield, hasAttemptedRiddle
-            roomState2[0] = new boolean[]{false, false};
-            // hasSword, hasAttemptedRiddle
-            roomState2[1] = new boolean[]{false, false};
-            // hasHelmet, hasAttemptedRiddle
-            roomState2[2] = new boolean[]{false, false};
-            // hasMetGhost, hasArmor, hasDrunkSpicyStew, hasAngeredGhost
-            roomState2[3] = new boolean[]{false, false, false, false};
-
-            roomNames1 = new String[]{"Southwest Tower", "Foyer", "Enchanting Room", "Southeast Tower", "Alchemy Room",
-                    "Courtyard", "Central Tower", "Hallway A", "Dining Room", "Kitchen", "Northwest Tower", "Library",
-                    "Hallway B", "Chapel", "Northeast Tower"};
-            roomDescriptions1 = new String[]
-                    {"You make your way into the southwestern tower. The Dining room lies north.",
-                            "You find yourself in the Foyer of a castle. There is set of stairs that bring you to the second floor of the castle. In the north, there is a Hallway and to the east lies the enchanting room",
-                            "You find yourself in the enchanting room. To the north, there is an alchemy room, to the south, there is the southeastern tower and to the west, there is the foyer of the castle",
-                            "You find yourself in the southeastern tower of the castle. To the north, there is an enchanting room.",
-                            "You make your way into the alchemy room. To the west lies a hallway and the enchanting room is south of here.",
-                            "You make your way into the courtyard. To the east lies the central tower and you can return into the castle through the west. Near the entrance of the central tower, you notice a skeleton that seemed to have been crawling for life in its final moments.",
-                            "You find yourself in the central tower. To the west, you can find courtyard",
-                            "You make your way into a hallway. To the north, it leads into another hallway and to the west lies a dining room.",
-                            "You make your way into a dining room. To the south lies the southwestern tower and the kitchen is to the north",
-                            "You find yourself in a kitchen. To the south lies the dining room, the Northwestern Tower to the north and a hallway to the east",
-                            "You make your way into the Northwestern Tower. To the south lies the kitchen.",
-                            "You find yourself in a library. To the south is a hallway and the chapel is to the east",
-                            "You make your way into a hallway. There lies a library to the north, a kitchen to the west and the alchemy room to the east",
-                            "You are in the chapel. To the west is the library and to the north is the Northeastern Tower",
-                            "You find yourself in the northeastern tower of the castle. To the south lies the chapel."};
-
-            if (roomState1[5][0] == true) {
-                roomDescriptions1[6] = "You make your way into the courtyard. To the east lies the central tower and you can return into the castle through the west. Near the entrance of the central tower, there remains the skeleton that you examined";
-            }
-
-            roomDirections1[0] = new int[]{8, -1, -1, -1};
-            roomDirections1[1] = new int[]{7, 2, -1, -1};
-            roomDirections1[2] = new int[]{4, -1, 3, 1};
-            roomDirections1[3] = new int[]{2, -1, -1, -1};
-            roomDirections1[4] = new int[]{-1, -1, 2, 12};
-            roomDirections1[5] = new int[]{-1, 6, -1, 7};
-            roomDirections1[6] = new int[]{-1, -1, -1, 5};
-            roomDirections1[7] = new int[]{12, 5, 1, 8};
-            roomDirections1[8] = new int[]{9, 7, 0, -1};
-            roomDirections1[9] = new int[]{10, 12, 8, -1};
-            roomDirections1[10] = new int[]{-1, -1, 9, -1};
-            roomDirections1[11] = new int[]{-1, 13, 12, -1};
-            roomDirections1[12] = new int[]{11, 4, 7, 9};
-            roomDirections1[13] = new int[]{14, -1, -1, 11};
-            roomDirections1[14] = new int[]{-1, -1, 13, -1};
-
-            roomNames2 = new String[]{"top floor of the southwestern tower", "top floor of the southeastern tower", "Bedroom A", "Bedroom B"};
-            roomDescriptions2 = new String[]
-                    {"You are on the top floor of the southwestern tower. You see a ghost in front of you.",
-                            "You are on the top floor of the southeastern tower. You see a ghost in front of you.",
-                            "You are on the top floor of the northeastern tower. You see a ghost in front of you.",
-                            "You are on the top floor of the northwestern tower. You see a ghost in front of you.",
-                            "You make your way onto the top floor of the Central Tower. A dormant knight stands there. His eyes suddenly open and he raises his sword, ready to strike you down."};
-
-            roomDirections2[0] = new int[]{-1, -1, -1, -1};
-            roomDirections2[1] = new int[]{-1, -1, -1, -1};
-            roomDirections2[2] = new int[]{-1, -1, -1, -1};
-            roomDirections2[3] = new int[]{-1, -1, -1, -1};
-            roomDirections2[4] = new int[]{-1, -1, -1, -1};
-
             int currentFloor = 1;
             int currentRoom = 1;
             int nextRoom = -1;
@@ -102,6 +26,8 @@
             String userAction = "";
             int userDialogueChooser;
             boolean done = false;
+
+            initializeArrays();
 
             System.out.println("You find yourself in a castle, unaware of your past and without any memory of who you are. " +
                     "Behind you lies the doors to leave, but they are locked, unable to be opened. " +
@@ -660,6 +586,80 @@
                     }
                 }
             }
+        }
+
+        public static void initializeArrays(){
+            // hasPotions, Alchemy ingredients
+            roomState1[4] = new boolean[]{false, false};
+            // isSkeletonExamined, Skeleton's Journal
+            roomState1[5] = new boolean[]{false, false};
+            // hasEaten plate of food
+            roomState1[8] = new boolean[]{false};
+            // Cooking Ingredients, hasCookedMeal, hasStew
+            roomState1[9] = new boolean[]{false, false, false};
+            // hasSwordUpgrade, hasArmorUpgrade, hasEnchantingKnowledge, hasAlchemyKnowledge
+            roomState1[11] = new boolean[]{false, false, false, false};
+            // hasReceivedBlessing
+            roomState1[13] = new boolean[]{false};
+
+            // hasShield, hasAttemptedRiddle
+            roomState2[0] = new boolean[]{false, false};
+            // hasSword, hasAttemptedRiddle
+            roomState2[1] = new boolean[]{false, false};
+            // hasHelmet, hasAttemptedRiddle
+            roomState2[2] = new boolean[]{false, false};
+            // hasMetGhost, hasArmor, hasDrunkSpicyStew, hasAngeredGhost
+            roomState2[3] = new boolean[]{false, false, false, false};
+
+            roomNames1 = new String[]{"Southwest Tower", "Foyer", "Enchanting Room", "Southeast Tower", "Alchemy Room",
+                    "Courtyard", "Central Tower", "Hallway A", "Dining Room", "Kitchen", "Northwest Tower", "Library",
+                    "Hallway B", "Chapel", "Northeast Tower"};
+            roomDescriptions1 = new String[]
+                    {"You make your way into the southwestern tower. The Dining room lies north.",
+                            "You find yourself in the Foyer of a castle. There is set of stairs that bring you to the second floor of the castle. In the north, there is a Hallway and to the east lies the enchanting room",
+                            "You find yourself in the enchanting room. To the north, there is an alchemy room, to the south, there is the southeastern tower and to the west, there is the foyer of the castle",
+                            "You find yourself in the southeastern tower of the castle. To the north, there is an enchanting room.",
+                            "You make your way into the alchemy room. To the west lies a hallway and the enchanting room is south of here.",
+                            "You make your way into the courtyard. To the east lies the central tower and you can return into the castle through the west. Near the entrance of the central tower, you notice a skeleton that seemed to have been crawling for life in its final moments.",
+                            "You find yourself in the central tower. To the west, you can find courtyard",
+                            "You make your way into a hallway. To the north, it leads into another hallway and to the west lies a dining room.",
+                            "You make your way into a dining room. To the south lies the southwestern tower and the kitchen is to the north",
+                            "You find yourself in a kitchen. To the south lies the dining room, the Northwestern Tower to the north and a hallway to the east",
+                            "You make your way into the Northwestern Tower. To the south lies the kitchen.",
+                            "You find yourself in a library. To the south is a hallway and the chapel is to the east",
+                            "You make your way into a hallway. There lies a library to the north, a kitchen to the west and the alchemy room to the east",
+                            "You are in the chapel. To the west is the library and to the north is the Northeastern Tower",
+                            "You find yourself in the northeastern tower of the castle. To the south lies the chapel."};
+
+            roomDirections1[0] = new int[]{8, -1, -1, -1};
+            roomDirections1[1] = new int[]{7, 2, -1, -1};
+            roomDirections1[2] = new int[]{4, -1, 3, 1};
+            roomDirections1[3] = new int[]{2, -1, -1, -1};
+            roomDirections1[4] = new int[]{-1, -1, 2, 12};
+            roomDirections1[5] = new int[]{-1, 6, -1, 7};
+            roomDirections1[6] = new int[]{-1, -1, -1, 5};
+            roomDirections1[7] = new int[]{12, 5, 1, 8};
+            roomDirections1[8] = new int[]{9, 7, 0, -1};
+            roomDirections1[9] = new int[]{10, 12, 8, -1};
+            roomDirections1[10] = new int[]{-1, -1, 9, -1};
+            roomDirections1[11] = new int[]{-1, 13, 12, -1};
+            roomDirections1[12] = new int[]{11, 4, 7, 9};
+            roomDirections1[13] = new int[]{14, -1, -1, 11};
+            roomDirections1[14] = new int[]{-1, -1, 13, -1};
+
+            roomNames2 = new String[]{"top floor of the southwestern tower", "top floor of the southeastern tower", "Bedroom A", "Bedroom B"};
+            roomDescriptions2 = new String[]
+                    {"You are on the top floor of the southwestern tower. You see a ghost in front of you.",
+                            "You are on the top floor of the southeastern tower. You see a ghost in front of you.",
+                            "You are on the top floor of the northeastern tower. You see a ghost in front of you.",
+                            "You are on the top floor of the northwestern tower. You see a ghost in front of you.",
+                            "You make your way onto the top floor of the Central Tower. A dormant knight stands there. His eyes suddenly open and he raises his sword, ready to strike you down."};
+
+            roomDirections2[0] = new int[]{-1, -1, -1, -1};
+            roomDirections2[1] = new int[]{-1, -1, -1, -1};
+            roomDirections2[2] = new int[]{-1, -1, -1, -1};
+            roomDirections2[3] = new int[]{-1, -1, -1, -1};
+            roomDirections2[4] = new int[]{-1, -1, -1, -1};
         }
 
         public static void finalBoss() {
